3325d73bc23b78176f334328b2d6b45f190d3139
Stanislaw Krowicki <krowicki00@gmail.com>
2021-11-18 18:20:16 +0100

    Added caret

--- components/TypingWindow.vue
+++ components/TypingWindow.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="main-window" @click="startTest">
-    <div class="words-container">
+    <div class="words-container" ref="words-container">
       <input type="text" id="words-input" ref="typing-input" v-on:keydown="keyPressed" autocomplete="off" autocapitalize="off" autocorrect="off">
+      <div v-if="displayCaret" ref="caret" id="caret"></div>
       <div v-for="word in words" :key="word" ref="words" class="word">
-        <div v-for="letter in word" :key="letter" ref="letters" class="letter">{{ letter }}</div>
+        <div v-for="letter in word" :key="letter" ref="letters" class="letter"><span>{{ letter }}</span></div>
+        <div class="fake-letter"></div>
       </div>
     </div>
   </div>
@@ -16,19 +18,47 @@
         words: ['age', 'eyes', 'wildblue', 'jingle', 'blues', 'californication', 'wet', 'sand', 'space', 'font', 'height', 'dependency'],
         currentWordIndex: 0,
         currentLetterIndex: 0,
-        correctLetters: 0
+        correctLetters: 0,
+        displayCaret: true,
       }
     },
 
     methods: {
       startTest() {
         this.$refs['typing-input'].focus()
+        this.moveCaret()
+      },
+
+      moveCaret() {
+        let caret = this.$refs.caret
+
+        if (!caret)
+          return 
+
+        let caretParent = caret.parentNode
+        if (!caretParent)
+          return 
+
+        let currentWord = this.$refs.words[this.currentWordIndex]
+        let currentLetter = currentWord.querySelectorAll('.letter')[this.currentLetterIndex]   
+
+        if (!currentLetter) {
+          let fakeLetter = currentWord.querySelector('.fake-letter')
+          caretParent.removeChild(caret)
+          fakeLetter.appendChild(caret)
+          return
+        }
+
+        caretParent.removeChild(caret)
+        currentLetter.appendChild(caret)
+        this.displayCaret = true
       },
 
       keyPressed(e) {
         if (e.code == 'Space') {
           this.currentWordIndex++
           this.currentLetterIndex = 0
+          this.moveCaret()
           return
         }
 
@@ -36,11 +66,11 @@
         if (word === undefined)
           return
 
-        let letter = word.children[this.currentLetterIndex]   
+        let letter = word.querySelectorAll('.letter')[this.currentLetterIndex]   
         if (letter === undefined)
           return
 
-        let expectedKey = letter.innerHTML
+        let expectedKey = letter.querySelector('span').innerHTML
 
         if (e.key == expectedKey) {
           this.correctLetters++
@@ -48,7 +78,8 @@
         } else 
           letter.className = `${letter.className} incorrect`
 
-        this.currentLetterIndex++;
+        this.currentLetterIndex++
+        this.moveCaret()
       }
     },
 
@@ -56,6 +87,8 @@
 </script>
 
 <style lang="scss" scoped>
+  $font-size: 4em;
+
   .main-window {
       width: 80em;
       height: 30em;
@@ -69,20 +102,36 @@
     margin-right: 1em;
   }
 
-  .letter {
-      display: inline-block;
+  .letter, .fake-letter {
+    display: inline-block;
+    position: relative;
+    span {
       color: $primary-color;
-      font-size: 4em;
+      font-size: $font-size;
       font-family: 'Shippori Antique', sans-serif;
       opacity: 0.4;
+    }
+    #caret {
+      position: absolute;
+      height: $font-size*1.5;
+      width: .5em;
+      background-color: $secondary-color;
+      opacity: 0.8;
+      top: 0;
+    }
   }
 
-  .correct {
+
+  .fake-letter {
+    position: absolute;
+  }
+
+  .correct > span{
     opacity: 0.8;
     color: $primary-color;
   }
 
-  .incorrect {
+  .incorrect > span{
     opacity: 0.5;
     color: $error-color;
   }
