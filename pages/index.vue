ea4ad514ac953bac709128984246f707610027b7
Stanislaw Krowicki <krowicki00@gmail.com>
2021-11-30 17:54:39 +0100

    Sketch of home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <TypingWindow />
+  <div class="main-container">
+    <TypingWindow id="typing-window" />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
+
+<style lang="scss" scoped>
+.main-container {
+  height: 90vh; // 10vh is taken by the navbar
+  background-color: $background-color;
+  overflow: hidden;
+}
+
+#typing-window {
+  display: block;
+  margin: 10em auto;
+  border-style: ridge;
+  border-color: $primary-color;
+}
+</style>
