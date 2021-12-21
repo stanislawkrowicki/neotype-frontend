ace0ab7e0752e2eff76225034c554860b94fb891
Stanislaw Krowicki <krowicki00@gmail.com>
2021-12-21 23:43:56 +0100

    Sketch of account page

---
+++ pages/account.vue
@@ -0,0 +1,106 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="user">
+        <div class="user-img"></div>
+        <p id="name">{{ name }}</p>
+      </div>
+      <div class="stats">
+        <p>Total tests: {{ tests }}</p>
+        <p>All time average: {{ avg }}</p>
+        <p>Member since: {{ memberSince }}</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      tests: 0,
+      avg: 0,
+      memberSince: "",
+    };
+  },
+
+  mounted() {
+    if (localStorage.getItem("token") === null) this.$router.push("/login");
+
+    let auth = "Bearer " + localStorage.getItem("token");
+    let config = {
+      headers: {
+        Authorization: auth,
+      },
+    };
+
+    this.$axios
+      .$get("/data", config)
+      .catch((error) => {
+        if (!error.response) this.$router.push("/");
+        if (error.response.status == 401) {
+          // Unauthorized
+          localStorage.removeItem("token");
+          this.$router.push("/login");
+        } else this.$router.push("/");
+      })
+      .then((response) => {
+        if (response === undefined) this.$router.push("/");
+        this.name = response.login;
+        this.tests = response.tests;
+        this.avg = response.avg;
+        this.memberSince = "todo";
+      });
+  },
+};
+</script>
+
+<style scoped lang="scss">
+main {
+  position: absolute;
+  height: 90vh;
+  width: 100vw;
+  background-color: $background-color;
+}
+
+.container {
+  margin-top: 5em;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 5vw;
+
+  @media only screen and (min-width: 680px) {
+    align-items: flex-start;
+    margin-left: 10vw;
+  }
+}
+
+.user {
+  .user-img {
+    height: 14em;
+    width: 14em;
+    background-color: #f0f;
+    margin-top: 0.9em;
+  }
+  #name {
+    font-size: 3em;
+    font-family: $font-family;
+    color: $primary-color;
+  }
+}
+
+.stats {
+  margin-left: 2em;
+  padding-right: 5vw;
+  font-size: 2em;
+  color: $primary-color;
+  font-family: $font-family;
+
+  p {
+    margin-top: 0;
+    margin-bottom: 2em;
+  }
+}
+</style>
